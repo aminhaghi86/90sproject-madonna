680945dbc7ea802a7495f81e11f2f5bd23c998a9
Amin.Haghi <90243818+AminFED23STO@users.noreply.github.com>
2022-09-16 04:57:15 +0200

    Madonna-project

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = "data.json";
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    data.forEach((element) => {
+      document.querySelector(".section-songs").innerHTML += `        
+    <div class="item item${element.id}">
+    <img src="${element.imagesrc}" alt="#" />
+    <h3>${element.nameofsong}</h3>
+    <a href="${element.audiosrc}">Download</a>
+    </div>`;
+    });
+  });
