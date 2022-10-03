d60cc93e2c5b591b650a3df3a074e0e0d0197c37
Amin.Haghi <90243818+AminFED23STO@users.noreply.github.com>
2022-10-03 22:46:56 +0200

    get value from user to create div or section based on user input

--- script.js
+++ script.js
@@ -16,19 +16,27 @@ fetch(url)
 //
 
 const addElements = () => {
-  if (valueInput != "" && textElement != "") {
-  let ds = document.getElementById("displayTag");
   let valueInput = document.getElementById("valueInput").value;
   let textElement = document.getElementById("textAreaBox").value;
-  let finalText = document.createTextNode(textElement);
-  let createElementValue = document.createElement(valueInput);
-  createElementValue.classList.add("hello");
-  createElementValue.appendChild(finalText);
-  
-    if (valueInput === "div" || "section") {
-   
-    
-      createElementValue.classList.toggle("hello-section");
+  if (valueInput != "" && textElement != "") {
+    let ds = document.getElementById("displayTag");
+    let finalText = document.createTextNode(textElement);
+    let createElementValue = document.createElement(valueInput);
+    createElementValue.classList.add("hello");
+    createElementValue.appendChild(finalText);
+
+    if (valueInput === "div" || valueInput==="section") {
+      let widthSD = prompt("Please Enter Width in Pixel");
+      let heightSD = prompt("Please Enter Height in Pixel");
+      let backGrundColor = prompt("Please Enter BackgroundColor");
+      console.log(widthSD);
+      console.log(heightSD);
+      console.log(backGrundColor);
+      // createElementValue.classList.toggle("hello-section");
+      
+      createElementValue.style.width = `${widthSD}px`;
+      createElementValue.style.height = `${heightSD}px`;
+      createElementValue.style.backgroundColor = `${backGrundColor}`;
     }
     console.log(textElement);
     ds.append(createElementValue);
